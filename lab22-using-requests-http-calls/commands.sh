6c4a0de1e11ec38bec4be70919a8b5f204750d87
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-29 19:37:07 +0530

    Create commands.sh

---
+++ lab22-using-requests-http-calls/commands.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Lab 22 - Using requests for HTTP Calls
+
+# Step 1: Install requests library
+pip install requests
+
+# Step 2: Create Python script
+nano http_requests.py
+
+# Step 3: Run the script
+python3 http_requests.py
