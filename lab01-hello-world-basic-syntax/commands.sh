f8b4f6f87aca4be4b895f6d6a2d9f5fefdf9d027
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-29 10:11:17 +0530

    Create commands.sh

---
+++ lab01-hello-world-basic-syntax/commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create Python file
+nano hello.py
+
+# List files
+ls
+
+# Run Python script
+python3 hello.py
+
+# Edit script to test indentation
+nano hello.py
+
+# Execute again
+python3 hello.py
