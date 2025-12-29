e4450638320827770952ea60f6dad23c94d34ec2
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-29 21:11:30 +0530

    Create quick_plot.py

---
+++ lab34-python-matplotlib-visualization/quick_plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [0, 1, 2, 3, 4, 5]
+y = [0, 1, 4, 9, 16, 25]
+
+# Create the plot
+plt.plot(x, y)
+
+# Add labels and title
+plt.title("Simple Line Plot")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+# Display the plot
+plt.show()
