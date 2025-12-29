9cbb73d0c142b173309035319c659f57153bf274
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-29 10:20:07 +0530

    Create arithmetic_operations.py

---
+++ lab03-arithmetic-expressions/arithmetic_operations.py
@@ -0,0 +1,25 @@
+# arithmetic_operations.py
+
+# Take user input
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Addition
+addition_result = number1 + number2
+print(f"Addition: {number1} + {number2} = {addition_result}")
+
+# Subtraction
+subtraction_result = number1 - number2
+print(f"Subtraction: {number1} - {number2} = {subtraction_result}")
+
+# Multiplication
+multiplication_result = number1 * number2
+print(f"Multiplication: {number1} * {number2} = {multiplication_result}")
+
+# Float division
+division_result = number1 / number2
+print(f"Division: {number1} / {number2} = {division_result}")
+
+# Integer division
+integer_division_result = number1 // number2
+print(f"Integer Division: {number1} // {number2} = {integer_division_result}")
