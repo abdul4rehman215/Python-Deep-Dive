c3b687fcfe3d802d87d6a8fa1649d32637cac7ed
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-29 10:36:36 +0530

    Create commands.sh

---
+++ lab11-file-io-basics/commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create Python scripts
+nano write_file.py
+nano read_file.py
+
+# Execute scripts
+python3 write_file.py
+python3 read_file.py
+
+# Verify file contents
+cat output.txt
