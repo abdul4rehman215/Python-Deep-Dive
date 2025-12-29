7490dba375f4d72851b7e5dd8e860c8fd8e32788
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-29 19:51:25 +0530

    Create commands.sh

---
+++ lab24-unit-testing-unittest/commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Lab 24 - Unit Testing with unittest
+
+# Step 1: Create the test file
+nano test_example.py
+
+# Step 2: Run tests using unittest discovery
+python3 -m unittest discover
