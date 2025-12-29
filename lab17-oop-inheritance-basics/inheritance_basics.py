083eafdcc288fbe9cc091c0ea6fd648cb9811f5b
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-29 11:32:33 +0530

    Create inheritance_basics.py

---
+++ lab17-oop-inheritance-basics/inheritance_basics.py
@@ -0,0 +1,30 @@
+# inheritance_basics.py
+
+# Parent class
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def describe_car(self):
+        print(f"{self.year} {self.make} {self.model}")
+
+# Child class
+class ElectricCar(Car):
+    def __init__(self, make, model, year, battery_size=75):
+        super().__init__(make, model, year)
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def describe_car(self):
+        super().describe_car()
+        self.describe_battery()
+
+# Create object
+my_car = ElectricCar("Tesla", "Model S", 2022)
+
+# Call methods
+my_car.describe_car()
